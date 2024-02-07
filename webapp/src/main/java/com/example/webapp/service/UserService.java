a0d4f6f558a7aa3618bf5bad2f9a5203e144c1ad
Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>
2024-02-07 16:46:41 -0500

    Added healthz resources
    Added healthz resources

--- webapp/src/main/java/com/example/webapp/service/UserService.java
+++ webapp/src/main/java/com/example/webapp/service/UserService.java
@@ -5,24 +5,50 @@ import com.example.webapp.dto.UserResponse;
 import com.example.webapp.entity.UserAccount;
 import com.example.webapp.repo.UserAccountRepo;
 import com.example.webapp.util.Utils;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class UserService implements UserDetailsService {
 
   private final UserAccountRepo userRepo;
   private final Utils utils;
   private final BCryptPasswordEncoder passwordEncoder;
+  private final DataSource ds;
 
-  public UserService(UserAccountRepo userRepo, Utils utils, BCryptPasswordEncoder passwordEncoder) {
+  public UserService(
+      UserAccountRepo userRepo, Utils utils, BCryptPasswordEncoder passwordEncoder, DataSource ds) {
     this.userRepo = userRepo;
     this.utils = utils;
     this.passwordEncoder = passwordEncoder;
+    this.ds = ds;
+  }
+
+
+  public ResponseEntity<Void> HealthCheckService() {
+    try (Connection conn = ds.getConnection()) {
+      Statement stmt = conn.createStatement();
+    } catch (SQLException ex) {
+      log.info(ex.getMessage());
+      return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+          .headers(noCacheHeaders())
+          .build();
+    }
+    return ResponseEntity.status(HttpStatus.OK).headers(noCacheHeaders()).build();
   }
 
   public UserResponse createUser(UserDTO userReq) {
