e7cd46e00d457bec26874f808a8d84ea430f2459
Jaswanth Marri <jeswanthjashu8@gmail.com>
2024-02-14 11:26:52 +0530

    Jaswanth feature (#42)
    * Initial commit
    
    Initial commit
    
    * Complete prjt
    
    Complete prjt
    
    * 111
    
    111
    
    * updated props
    
    updated props
    
    * Added healthz resources
    
    Added healthz resources
    
    * changes
    
    changes
    
    * Update README.md
    
    * Update maven.yml
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update maven.yml
    
    * Update README.md
    
    * Update SecurityBeansConfig.java
    
    * Update maven.yml
    
    * Update SecurityBeansConfig.java
    
    * Update maven.yml
    
    * Update SecurityBeansConfig.java
    
    ---------
    
    Co-authored-by: Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>

--- webapp/src/main/java/com/example/webapp/config/SecurityBeansConfig.java
+++ webapp/src/main/java/com/example/webapp/config/SecurityBeansConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
+
+
 @Configuration
 public class SecurityBeansConfig {
 
