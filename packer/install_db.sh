fecde8f8788b1cebaf237446a03f91b8e91d88c4
Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>
2024-02-21 17:39:42 -0500

    Update install_db.sh

--- packer/install_db.sh
+++ packer/install_db.sh
@@ -26,13 +26,13 @@ sudo firewall-cmd --add-port=5432/tcp --permanent || exit 1
 sudo firewall-cmd --reload || exit 1
 
 # Check if the role exists before creating it
-if sudo psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='postgres'" | grep -q 1; then
+if sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='postgres'" | grep -q 1; then
 	#sudo psql -c "ALTER USER postgres WITH PASSWORD 'postgres';" || exit 1
     echo "Role 'postgres' already exists."
 else
     # Create database and user (use stronger password)
-    sudo psql -c "CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres';" || exit 1
-    sudo psql -c "ALTER ROLE postgres CREATEDB;" || exit 1
+    sudo -u postgres psql -c "CREATE ROLE postgres WITH LOGIN PASSWORD 'postgres';" || exit 1
+    sudo -u postgres psql -c "ALTER ROLE postgres CREATEDB;" || exit 1
 fi
 
 sudo -u postgres createdb test_db
@@ -41,13 +41,13 @@ sudo -u postgres createdb test_db
 if sudo psql -lqt | cut -d \| -f 1 | grep -qw test_db; then
     echo "Database 'test_db' already exists."
 else
-    sudo psql -c "CREATE DATABASE test_db;" || exit 1
+    sudo -u postgres psql -c "CREATE DATABASE test_db;" || exit 1
 fi
 
 #sudo chown -R csye6225:csye6225 /var/
 
 # Grant privileges
-sudo psql -c "GRANT ALL PRIVILEGES ON DATABASE test_db TO postgres;" || exit 1
+sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE test_db TO postgres;" || exit 1
 
 # Log execution details
 echo "PostgreSQL installation and configuration complete." >> /var/log/postgresql_setup.log
