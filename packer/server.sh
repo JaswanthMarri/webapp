60af5dd6a8114fc36677e3b93631f18e97928ec5
Jaswanth Marri <jeswanthjashu8@gmail.com>
2024-02-22 00:08:05 +0530

    Asgn 4 feature (#96)
    * Initial commit
    
    Initial commit
    
    * Complete prjt
    
    Complete prjt
    
    * 111
    
    111
    
    * updated props
    
    updated props
    
    * Added healthz resources
    
    Added healthz resources
    
    * changes
    
    changes
    
    * Update README.md
    
    * Update maven.yml
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update maven.yml
    
    * Update README.md
    
    * Update SecurityBeansConfig.java
    
    * Update maven.yml
    
    * Update SecurityBeansConfig.java
    
    * Update maven.yml
    
    * Update SecurityBeansConfig.java
    
    * asgn-3 commit
    
    asgn-3 commit
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Create test.yml
    
    * Update maven.yml
    
    * tests
    
    tests
    
    * Update test.yml
    
    * db props
    
    * Update application.properties
    
    * final commit
    
    * Update maven.yml
    
    * Delete .github/workflows/test.yml
    
    * Update README.md
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update pom.xml
    
    * packer files
    
    * Update maven.yml
    
    * Update maven.yml
    
    * upgrade to hcl
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update pck-img.json.pkr.hcl
    
    * Update pck-img.json.pkr.hcl
    
    * Update pck-img.json.pkr.hcl
    
    * Update pck-img.json.pkr.hcl
    
    * Update pck-img.json.pkr.hcl
    
    * Update pck-img.json.pkr.hcl
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Update maven.yml
    
    * Create gcp.yml
    
    * Update README.md
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Update README.md
    
    * Update gcp.yml
    
    * Update gcp.yml
    
    * Delete .github/workflows/db.yml
    
    * Update test.yml
    
    * Update packer.yml
    
    * Update packer.yml
    
    * Update packer.yml
    
    * Update pck-img.json.pkr.hcl
    
    * Update packer.yml
    
    * Update pck-img.json.pkr.hcl
    
    * Update packer.yml
    
    * Update pck-img.json.pkr.hcl
    
    * Update pck-img.json.pkr.hcl
    
    * Update packer.yml
    
    * Update packer.yml
    
    * Update pck-img.json.pkr.hcl
    
    * Update packer.yml
    
    * Update packer.yml
    
    * Update pck-img.json.pkr.hcl
    
    * Update packer.yml
    
    * Update packer.yml
    
    * Update packer.yml
    
    * Update pck-img.json.pkr.hcl
    
    * prop file and service file edited
    
    * Update csye6225.service
    
    * Update server.sh
    
    ---------
    
    Co-authored-by: Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>

--- packer/server.sh
+++ packer/server.sh
@@ -2,6 +2,7 @@
 
 ls -l
 sudo cp /tmp/csye6225.service /etc/systemd/system/csye6225.service
+sudo mkdir /opt/app
 sudo cp /tmp/webapp-0.0.1-SNAPSHOT.jar /opt/app/webapp-0.0.1-SNAPSHOT.jar
 sudo cp /tmp/application.properties /opt/application.properties
 cd /tmp
