fa88e88e1ca351037a013ac758de202bad270a09
Jaswanth Marri <jeswanthjashu8@gmail.com>
2024-03-19 19:23:32 +0530

    Assgn 6 (#139)
    * ops-agent installation script
    
    ops-agent installation script
    
    * packer ops config
    
    packer ops config
    
    * Update ops_agent.sh
    
    ---------
    
    Co-authored-by: Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>

--- packer/ops_agent.sh
+++ packer/ops_agent.sh
@@ -3,6 +3,6 @@
 curl -sSO https://dl.google.com/cloudagents/add-google-cloud-ops-agent-repo.sh
 sudo bash add-google-cloud-ops-agent-repo.sh --also-install
 
-sudo cp /tmp/config.yml /etc/google-cloud-ops-agent/config.yml
+sudo cp /tmp/config.yml /etc/google-cloud-ops-agent/config.yaml
 sudo systemctl restart google-cloud-ops-agent
 
