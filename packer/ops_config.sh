e45d2f72bbf32bd137746ad55f26448252801b6a
Jaswanth Marri <jeswanthjashu8@gmail.com>
2024-03-19 20:12:12 +0530

    Assgn 6 (#142)
    * ops-agent installation script
    
    ops-agent installation script
    
    * packer ops config
    
    packer ops config
    
    * Update ops_agent.sh
    
    * Update pck-img.json.pkr.hcl
    
    * ops-config
    
    ops-config
    
    * Update ops_config.sh
    
    ---------
    
    Co-authored-by: Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>

--- packer/ops_config.sh
+++ packer/ops_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo cp /tmp/config.yml /etc/google-cloud-ops-agent/config.yaml
-sudo systemctl restart google-cloud-ops-agent
+
+sudo systemctl enable google-cloud-ops-agent
+sudo systemctl start google-cloud-ops-agent
 
