abc76efe8b5c8aa35eaa1ca623a7d321ef50bb11
Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>
2024-03-20 10:35:46 -0400

    Update ops_config.sh

--- packer/ops_config.sh
+++ packer/ops_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp /tmp/config.yml /etc/google-cloud-ops-agent/config.yaml
+sudo mv /tmp/config.yml /etc/google-cloud-ops-agent/config.yaml
 
 sudo systemctl enable google-cloud-ops-agent
 sudo systemctl start google-cloud-ops-agent
