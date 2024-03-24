2271ce1b383bc383db4694baaddffe935f945b72
Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>
2024-03-24 17:41:46 -0400

    skip tests

--- webapp/src/test/java/com/example/webapp/WebappApplicationTests.java
+++ webapp/src/test/java/com/example/webapp/WebappApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.webapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("qa")
 class WebappApplicationTests {
 
 	@Test
