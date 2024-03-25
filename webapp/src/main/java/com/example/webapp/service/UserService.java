558d282c12a0fb19bbe31c2d842fff65829cb6e6
Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>
2024-03-25 10:24:49 -0400

    Update UserService.java

--- webapp/src/main/java/com/example/webapp/service/UserService.java
+++ webapp/src/main/java/com/example/webapp/service/UserService.java
@@ -124,8 +124,9 @@ public class UserService implements UserDetailsService, UserDetailsPasswordServi
       if (LocalDateTime.now().isAfter(user.getTokenExpTime())) {
         return false;
       }
+      
       user.setIsVerfied(true);
     }
-    return true;
+    return false;
   }
 }
