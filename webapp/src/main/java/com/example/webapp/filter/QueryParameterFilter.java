9071379afbab6db0c055446b54d5ee839dbca0d4
Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>
2024-03-25 10:19:59 -0400

    Update QueryParameterFilter.java

--- webapp/src/main/java/com/example/webapp/filter/QueryParameterFilter.java
+++ webapp/src/main/java/com/example/webapp/filter/QueryParameterFilter.java
@@ -7,26 +7,26 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-
-@Component
-public class QueryParameterFilter implements Filter {
-
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    HttpServletRequest httpRequest = (HttpServletRequest) request;
-    HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-    if (httpRequest.getQueryString() != null) {
-      // Reject the request with a custom error response
-      ResponseEntity<String> errorResponse =
-          ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-      httpResponse.setStatus(HttpStatus.BAD_REQUEST.value());
-      // httpResponse.getWriter().write(errorResponse));
-      return;
-    }
-
-    // If no query parameters are present, proceed with the request
-    chain.doFilter(request, response);
-  }
-}
+//
+//@Component
+//public class QueryParameterFilter implements Filter {
+//
+//  @Override
+//  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+//      throws IOException, ServletException {
+//    HttpServletRequest httpRequest = (HttpServletRequest) request;
+//    HttpServletResponse httpResponse = (HttpServletResponse) response;
+//
+//    if (httpRequest.getQueryString() != null) {
+//      // Reject the request with a custom error response
+//      ResponseEntity<String> errorResponse =
+//          ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//      httpResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+//      // httpResponse.getWriter().write(errorResponse));
+//      return;
+//    }
+//
+//    // If no query parameters are present, proceed with the request
+//    chain.doFilter(request, response);
+//  }
+//}
