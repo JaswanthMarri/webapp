c7099260d7ff8a78e326ebadc76c4cfa2a8e6a19
Jaswanth Marri <jeswanthjashu8@gmail.com>
2024-04-03 23:06:19 +0530

    Assg 8 (#196)
    * logging added
    
    * forbidden updated
    
    * transactional annotation
    
    ---------
    
    Co-authored-by: Jaswanth Marri <145034631+Jaswanth-marri@users.noreply.github.com>

--- webapp/src/main/java/com/example/webapp/config/SecurityConfig.java
+++ webapp/src/main/java/com/example/webapp/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @Configuration
@@ -45,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    return new AuthFailureHandler();
 //  }
 
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf()
@@ -61,4 +63,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         //.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
         .httpBasic(); // Enable basic authentication
   }
+
+
 }
